75fbe4cdb8c7602f0764358e1ac1a978d5952e0b
Rebellion Git <rebellion.git@gmail.com>
2025-02-18 18:20:40 -0600

    idk
    Co-Authored-By: Taha Nil <tnil.programming@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
@@ -181,6 +182,12 @@ public class RobotContainer {
     // Switch to X pattern when X button is pressed
     controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
+    copilotController.a().onTrue(
+        new ParallelCommandGroup(
+            
+        )
+        new ElevatorPIDCommand(Positions.L4, elevator)
+    )
     // Reset gyro to 0° when B button is pressed
     controller
         .b()
--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -21,7 +20,7 @@ public class IntakeConstants {
 
   public static class OTBIntake {
     public static int kLeftPivotID = 51;
-    public static int kRightPivotID = 53; 
+    public static int kRightPivotID = 53;
     public static int kLeftRollerID = 52;
     public static int kRightRollerID = 54;
     public static MotorType kMotorType = MotorType.kBrushless;
@@ -33,14 +32,13 @@ public class IntakeConstants {
     public static double kVelocityConversionFactor = kPositionConversionFactor / 60;
     public static double kLeftEncoderOffsetRev = 0;
     public static double kRightEncoderOffsetRev = 0;
-    
 
     public static final SparkMaxConfig kLeftPivotConfig = new SparkMaxConfig();
     public static final SparkMaxConfig kRightPivotConfig = new SparkMaxConfig();
     public static final SparkMaxConfig kRollerConfig = new SparkMaxConfig();
 
     static {
-      //kLeftPivotConfig.idleMode(kPivotIdleMode).smartCurrentLimit(kCurrentLimit);
+      // kLeftPivotConfig.idleMode(kPivotIdleMode).smartCurrentLimit(kCurrentLimit);
       kRightPivotConfig.idleMode(kPivotIdleMode).smartCurrentLimit(kPivotCurrentLimit);
       kRollerConfig.idleMode(kRollerIdleMode).smartCurrentLimit(kRollerCurrentLimit);
       // kLeftPivotConfig
